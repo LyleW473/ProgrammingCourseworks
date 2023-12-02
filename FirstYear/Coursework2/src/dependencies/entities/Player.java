455227de18cebbc7db18114f48b3d847e8e7d332
Lyle Wong <wlyle473@gmail.com>
2023-12-02 18:29:25 +0000

    Added comments to Player class

--- FirstYear/Coursework2/src/dependencies/entities/Player.java
+++ FirstYear/Coursework2/src/dependencies/entities/Player.java
@@ -5,32 +5,47 @@ import java.util.ArrayList;
 public class Player 
 {
     private Room currentRoom;
-
     private ArrayList<Artifact> inventory = new ArrayList<Artifact>();
-    private double inventoryWeight = 0.0;
+    private double inventoryWeight = 0.0; // Current total weight of the player's inventory
     public final double INVENTORY_WEIGHT_LIMIT = 5.0; // Total amount of weight that the player can have
     private ArrayList<Room> roomsHistory = new ArrayList<Room>(); // Stores the history of all the rooms the player has visited (in order)
 
+    /**
+     * @return the current room that the player is in
+     */
     public Room getCurrentRoom()
     {
         return currentRoom;
     }
 
+    /**
+     * Sets the current room the player is in to the selected room
+     * @param selectedRoom is the room which is selected to be the current room the player is in
+     */
     public void setCurrentRoom(Room selectedRoom)
     {
         currentRoom = selectedRoom;
     }
 
+    /**
+     * @return the inventory containing all of the player's collected artifacts
+     */
     public ArrayList<Artifact> getInventory()
     {
         return inventory;
     }
 
+    /**
+     * @return the current total weight of the player's inventory
+     */
     public double getInventoryWeight()
     {
         return inventoryWeight;
     }
 
+    /**
+     * Prints out: the details of each artifact inside of the player's inventory, current weight of inventory, weight limit set for the inventory, in the terminal
+     */
     public void showInventoryContents()
     {
         // Display details for each artifact inside of the inventory
@@ -43,7 +58,12 @@ public class Player
         System.out.println("Current total weight: " + inventoryWeight);
         System.out.println("Weight limit: " + INVENTORY_WEIGHT_LIMIT);
     }
-    
+
+    /**
+     * Adds an artifact to the player's inventory, and updates the inventory weight accordingly
+     * @param artifactToCollect is the artifact that the player just collected, which will be added to the inventory
+     * @param newWeight is the new weight of the inventory, after adding the artifact to collect
+     */
     public void addToInventory(double newWeight, Artifact artifactToCollect)
     {
         inventoryWeight = newWeight;
@@ -51,6 +71,10 @@ public class Player
         System.out.println("The '" + artifactToCollect.getName() +  "' artifact has been added to your inventory!");
     }
     
+    /**
+     * Removes an artifact from the player's inventory, and updates the inventory weight accordingly
+     * @param itemIndex is the index the player used in their "drop" command, specifying which artifact to drop.
+     */
     public Artifact removeFromInventory(int itemIndex)
     {
         Artifact artifactToDrop = inventory.get(itemIndex);
@@ -77,7 +101,7 @@ public class Player
     }
 
     /**
-     * @return the last room inside of the room history (i.e., the last room the player was in)
+     * @return the last room inside of the room history (i.e., the previous room the player was in)
      */
     public Room returnPrevious()
     {      
@@ -99,7 +123,7 @@ public class Player
     }
 
     /**
-     * Clears the player's room history
+     * Clears the history of rooms that the player has visited
      * - Executed after the player enters the magic attic / transporter room.
      * - This is an intended side effect of teleporting.
      */
