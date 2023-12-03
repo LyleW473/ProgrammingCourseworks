7fd97d2453e37a5c27f2f78a249422600047238c
Lyle Wong <wlyle473@gmail.com>
2023-12-03 20:04:43 +0000

    Changes made: - Refined documentation for the Room class. - Re-organised methods within the class.

--- FirstYear/Coursework2/src/dependencies/entities/Room.java
+++ FirstYear/Coursework2/src/dependencies/entities/Room.java
@@ -19,19 +19,20 @@ import java.util.HashMap;
 
 public class Room 
 {
-    private static ArrayList<Room> NPCSpawnableRooms = new ArrayList<Room>(); // Stores all the rooms that NPCs can spawn in
-    private static ArrayList<Room> artifactSpawnableRooms = new ArrayList<Room>(); // Stores all the rooms that artifacts can spawn in
-    private static ArrayList<Room> allRooms = new ArrayList<Room>(); // List of all rooms
-    private static Room magicTransporterRoom; // Pointer to Room set as the magic transporter room
-    private static Room goalRoom; // Pointer to Room that the player needs to drop all artifacts inside in order to win
+    private static ArrayList<Room> NPCSpawnableRooms = new ArrayList<Room>(); // Stores all the rooms that NPCs can spawn in.
+    private static ArrayList<Room> artifactSpawnableRooms = new ArrayList<Room>(); // Stores all the rooms that artifacts can spawn in.
+    private static ArrayList<Room> allRooms = new ArrayList<Room>(); // List of all room objects instantiated.
+    private static Room magicTransporterRoom; // Pointer to Room set as the magic transporter room.
+    private static Room goalRoom; // Pointer to Room that the player needs to drop all artifacts inside in order to win.
 
-    private String description;
+    private String description; // Description of this room.
     private HashMap<String, Room> exits = new HashMap<String, Room>(); // Stores the exits of this room.
-    private NPC assignedNPC; // Pointer to NPC assigned to this room
-    private Artifact assignedArtifact; // Pointer to Artifact assigned to this room
+    private NPC assignedNPC; // Pointer to NPC assigned to this room.
+    private Artifact assignedArtifact; // Pointer to Artifact assigned to this room.
 
     /**
-     * Creates a room object
+     * Constructor for Room class. 
+     * Creates and initialises a Room object with a description and whether or not NPCs or artifacts can spawn in them.
      * @param description The room's description (e.g., "in the kitchen" or "outside the Smith's residence")
      * @param npcSpawnable Whether or not NPCs can spawn in this room
      * @param artifactSpawnable Whether or not artifacts can spawn  in this room
@@ -40,31 +41,22 @@ public class Room
     {
         this.description = description;
 
-        // Add this room to the NPCSpawnable rooms list if applicable
+        // Add this room to the NPCSpawnable rooms list, if applicable
         if (npcSpawnable == true)
         {
             Room.NPCSpawnableRooms.add(this);
         }
 
-        // Add this room to the artifactSpawnable rooms list if applicable
+        // Add this room to the artifactSpawnable rooms list, if applicable
         if (artifactSpawnable == true)
         {
             Room.artifactSpawnableRooms.add(this);
         }
 
-        // Add to list of all rooms (To teleport the player when entering the magic attic)
+        // Add this room to the list of all rooms
         Room.allRooms.add(this);
     }
     
-    /**
-     * Sets a room as the magic transporter room
-     * @param selectedRoom The room (object) to set as the magic transporter room
-     */
-    public static void setMagicTransporterRoom(Room selectedRoom)
-    {
-        Room.magicTransporterRoom = selectedRoom;
-    }
-
     /**
      * @return A boolean indicating whether or not the room passed in is the magic transporter room
      * @param RoomToCheck The room to check against
@@ -75,7 +67,16 @@ public class Room
     }
 
     /**
-     * @return An ArrayList<Room> containing all of the room (objects) created in the world
+     * @return A boolean indicating whether or not the room passed in is the goal room (i.e., the room that the player needs to drop all artifacts in, to win).
+     * @param roomToCheck The room to check against
+     */
+    public static boolean isGoalRoom(Room roomToCheck)
+    {
+        return roomToCheck.equals(Room.goalRoom);
+    }
+
+    /**
+     * @return An ArrayList<Room> containing all of the room (objects) created in the world.
      */
     public static ArrayList<Room> getAllRooms()
     {
@@ -83,7 +84,32 @@ public class Room
     }
 
     /**
-     * Sets a passed-in room as the goal room
+     * @return An ArrayList<Room> containing the rooms that NPCs can spawn in.
+     */
+    public static ArrayList<Room> getNPCSpawnableRooms()
+    {
+        return Room.NPCSpawnableRooms;
+    }
+
+    /**
+     * @return An ArrayList<Room> containing the rooms that artifacts can spawn in.
+     */
+    public static ArrayList<Room> getArtifactSpawnableRooms()
+    {
+        return Room.artifactSpawnableRooms;
+    }
+
+    /**
+     * Sets a passed-in room as the magic transporter room.
+     * @param selectedRoom The room (object) to set as the magic transporter room
+     */
+    public static void setMagicTransporterRoom(Room selectedRoom)
+    {
+        Room.magicTransporterRoom = selectedRoom;
+    }
+
+    /**
+     * Sets a passed-in room as the goal room.
      * @param selectedRoom The room (object) to set as the goal room
      */
     public static void setGoalRoom(Room selectedRoom)
@@ -92,42 +118,7 @@ public class Room
     }
 
     /**
-     * @return A boolean indicating whether or not the room passed in is the goal room (i.e., the room that the player needs to drop all artifacts in to win)
-     * @param RoomToCheck The room to check against
-     */
-    public static boolean isGoalRoom(Room roomToCheck)
-    {
-        return roomToCheck.equals(Room.goalRoom);
-    }
-
-    /**
-     * @return An ArrayList<Room> containing the rooms that NPCs can spawn in
-     */
-    public static ArrayList<Room> getNPCSpawnableRooms()
-    {
-        return Room.NPCSpawnableRooms;
-    }
-
-    /**
-     * @return An ArrayList<Room> containing the rooms that artifacts can spawn in
-     */
-    public static ArrayList<Room> getArtifactSpawnableRooms()
-    {
-        return Room.artifactSpawnableRooms;
-    }
-
-    /**
-     * Define an exit from this room.
-     * @param direction The direction of the exit.
-     * @param neighbor  The room to which the exit leads.
-     */
-    public void setExit(String direction, Room neighbor) 
-    {
-        exits.put(direction, neighbor);
-    }
-
-    /**
-     * @return The short description of the room
+     * @return The short description of the room.
      * (the one that was defined in the constructor).
      */
     public String getShortDescription()
@@ -139,7 +130,8 @@ public class Room
      * Return a description of the room in the form:
      *      You are in the main hallway.
      *      Exits: outside (0) | living room (1) | kitchen (2) | bathroom (3)
-     * @return A long description of this room
+     * @return A long description of this room.
+     * @param options A list of options (i.e., exits) available to the player.
      */
     public String getLongDescription(ArrayList<String> options)
     {
@@ -150,7 +142,7 @@ public class Room
      * Adds all the options (exits) available to the player, and returns a string containing the exits available to the player when called.
      * @return A string describing the rooms this room connects to / details of the room's exits, e.g.
      *      Exits: outside (0) | living room (1) | kitchen (2) | bathroom (3)
-     * @param options A list of options (i.e., exits) available to the player
+     * @param options A list of options (i.e., exits) available to the player.
      */
     private String getExitString(ArrayList<String> options)
     {
@@ -158,15 +150,16 @@ public class Room
         int i = 0;
         int numExits = exits.size(); 
 
+        // For each exit for this room
         for(String exit : exits.keySet()) {
-            returnString += exit;
-            returnString += " (" + i + ")";
+            returnString += exit; // Add the name of the room to the return string
+            returnString += " (" + i + ")"; // Add the "option number" to the return string
 
             options.add(exit); // Add this exit to the available options the player currently has
             
             if (i < numExits - 1)
             {   
-                returnString += " | ";
+                returnString += " | "; // Add a separator to the return string
             }
             i ++;
         }
@@ -174,7 +167,7 @@ public class Room
     }
 
     /**
-     * Return the room that is reached if we go from this room in direction
+     * Return the room that is reached if we go from this room in direction.
      * "direction". If there is no room in that direction, return null.
      * @param direction The exit's direction.
      * @return The room in the given direction.
@@ -185,7 +178,7 @@ public class Room
     }
 
     /**
-     * @return A boolean indicating whether an NPC is inside this room or not
+     * @return A boolean indicating whether an NPC is inside this room or not.
      */
     public boolean hasNPC()
     {
@@ -193,7 +186,7 @@ public class Room
     }
 
     /**
-     * @return A boolean indicating whether an artifact is inside this room or not
+     * @return A boolean indicating whether an artifact is inside this room or not.
      */
     public boolean hasArtifact()
     {
@@ -201,7 +194,8 @@ public class Room
     }
 
     /**
-     * @return The assigned NPC to this room (should only be called if the assigned NPC is not null)
+     * @return The assigned NPC to this room.
+     * (Should only be called if the assigned NPC is not null)
      */
     public NPC getAssignedNPC()
     {   
@@ -209,8 +203,27 @@ public class Room
     }
 
     /**
-     * Assigns a passed-in NPC to this room
-     * @param NPCToAssign is the NPC to set as the assignedNPC for this room
+     * @return The assigned artifact to this room.
+     * (Should only be called if the assigned artifact is not null)
+     */
+    public Artifact getAssignedArtifact()
+    {
+        return assignedArtifact;
+    }
+
+    /**
+     * Defines an exit from this room.
+     * @param direction The direction of the exit.
+     * @param neighbor  The room to which the exit leads.
+     */
+    public void setExit(String direction, Room neighbor) 
+    {
+        exits.put(direction, neighbor);
+    }
+
+    /**
+     * Assigns a passed-in NPC to this room.
+     * @param NPCToAssign The NPC to set as the assignedNPC for this room.
      */
     public void assignNPC(NPC NPCToAssign) 
     {
@@ -218,20 +231,11 @@ public class Room
     }
 
     /**
-     * Assigns a passed-in artifact to this room
-     * @param artifactToAssign is the artifact to set as the assignedArtifact for this room
+     * Assigns a passed-in artifact to this room.
+     * @param artifactToAssign The artifact to set as the assignedArtifact for this room.
      */
     public void assignArtifact(Artifact artifactToAssign) 
     {
         assignedArtifact = artifactToAssign;
-    }
-
-    /**
-     * @return The assigned artifact to this room 
-     * (Should only be called if the assigned artifact is not null)
-     */
-    public Artifact getAssignedArtifact()
-    {
-        return assignedArtifact;
     }
 }
