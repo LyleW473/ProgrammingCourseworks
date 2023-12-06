7d52b8ee30fd1780f7486719d500ec808dc419b4
Lyle Wong <wlyle473@gmail.com>
2023-12-06 02:13:11 +0000

    Fixing note in Parser

--- FirstYear/Coursework2/Parser.java
+++ FirstYear/Coursework2/Parser.java
@@ -47,7 +47,7 @@ public class Parser
 
         inputLine = reader.nextLine();
 
-        // Find up to two words on the line.
+        // Find up to 3 words on the line.
         Scanner tokenizer = new Scanner(inputLine);
         if(tokenizer.hasNext()) {
             word1 = tokenizer.next();      // get first word
